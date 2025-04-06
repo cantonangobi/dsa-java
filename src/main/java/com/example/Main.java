2af1f24c1d4d06c7df5e8e1efa92cd3d8573a7bf
Cantona Ngobi <cantonangobi@gmail.com>
2025-04-06 08:55:13 +0300

    finalized the linked list

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -14,27 +14,153 @@ public class Main {
 
         LinkedList<Integer> list = new LinkedList<>();
         System.out.println(list);
+        System.out.println("Size: " + list.size());
         System.out.println("Empty? " + list.isEmpty());
         System.out.println();
         
-        list.add(element1);
+        list.pushBack(element1);
         System.out.println(list);
+        System.out.println("Size: " + list.size());
         System.out.println("Empty? " + list.isEmpty());
         System.out.println();
 
-        list.add(element2);
+        list.pushBack(element2);
         System.out.println(list);
+        System.out.println("Size: " + list.size());
         System.out.println("Empty? " + list.isEmpty());
         System.out.println();
 
-        list.add(element3);
-        list.add(element4);
+        list.pushBack(element3);
+        list.pushBack(element4);
         System.out.println(list);
+        System.out.println("Size: " + list.size());
         System.out.println("Empty? " + list.isEmpty());
         System.out.println();
 
+        int num = 4;
+        System.out.println("Is " + num + " present? " + list.contains(num));
+        
+        num = 5;
+        System.out.println("Is " + num + " present? " + list.contains(num));
 
+        
+        num = 6;
+        System.out.println("Is " + num + " present? " + list.contains(num));
 
+        
+        num = 8;
+        System.out.println("Is " + num + " present? " + list.contains(num));
+
+        
+        num = 9;
+        System.out.println("Is " + num + " present? " + list.contains(num));
+
+        System.out.println();
+
+        int index = 0;
+        System.out.println("Element at " + index + " = " + list.getAt(index));
+
+        index = 1;
+        System.out.println("Element at " + index + " = " + list.getAt(index));
+
+        index = 2;
+        System.out.println("Element at " + index + " = " + list.getAt(index));
+
+        index = 3;
+        System.out.println("Element at " + index + " = " + list.getAt(index));
+
+        System.out.println();
+        index = 0;
+        list.setAt(index, 9);
+        System.out.println("Element at " + index + " = " + list.getAt(index));
+
+        index = 1;
+        list.setAt(index, 10);
+        System.out.println("Element at " + index + " = " + list.getAt(index));
+
+        index = 2;
+        list.setAt(index, 11);
+        System.out.println("Element at " + index + " = " + list.getAt(index));
+
+        index = 3;
+        list.setAt(index, 12);
+        System.out.println("Element at " + index + " = " + list.getAt(index));
+
+        System.out.println();
+
+        System.out.println(list);
+        System.out.println("Size: " + list.size());
+        System.out.println("Empty? " + list.isEmpty());
+        num = 11;
+        System.out.println("Is " + num + " present? " + list.contains(num));
+        System.out.println();
+
+        list.deleteAt(2);
+        System.out.println(list);
+        System.out.println("Size: " + list.size());
+        System.out.println("Empty? " + list.isEmpty());
+        num = 11;
+        System.out.println("Is " + num + " present? " + list.contains(num));
+        System.out.println();
+
+        list.pushBack(13);
+        System.out.println(list);
+        System.out.println("Size: " + list.size());
+        System.out.println("Empty? " + list.isEmpty());
+        System.out.println();
+
+        list.deleteAt(0);
+        System.out.println(list);
+        System.out.println("Size: " + list.size());
+        System.out.println("Empty? " + list.isEmpty());
+        num = 13;
+        System.out.println("Is " + num + " present? " + list.contains(num));
+        System.out.println();
+
+        list.deleteAt(2);
+        System.out.println(list);
+        System.out.println("Size: " + list.size());
+        System.out.println("Empty? " + list.isEmpty());
+        num = 13;
+        System.out.println("Is " + num + " present? " + list.contains(num));
+        System.out.println();
+
+        System.out.println(list);
+        System.out.println("Size: " + list.size());
+        System.out.println();
+        
+        list.insertAt(0, 9);
+        System.out.println(list);
+        System.out.println("Size: " + list.size());
+        System.out.println();
+
+        list.insertAt(2, 11);
+        System.out.println(list);
+        System.out.println("Size: " + list.size());
+        System.out.println();
+
+        list.insertAt(4, 13);
+        System.out.println(list);
+        System.out.println("Size: " + list.size());
+        System.out.println();
+        
+        list.delete(11);
+        System.out.println(list);
+        System.out.println("Size: " + list.size());
+
+        list.delete(13);
+        System.out.println(list);
+        System.out.println("Size: " + list.size());
+
+        list.insertAt(0, 8);
+        list.pushFront(7);
+        System.out.println(list);
+        System.out.println("Size: " + list.size());
+
+        System.out.println("Index of 7 is " + list.indexOf(7));
+        System.out.println("Index of 9 is " + list.indexOf(9));
+        System.out.println("Index of 10 is " + list.indexOf(10));
+        System.out.println("Index of 12 is " + list.indexOf(12));
         
     }
 }
--- src/main/java/com/example/linkedlist/LinkedList.java
+++ src/main/java/com/example/linkedlist/LinkedList.java
@@ -19,6 +19,22 @@ public class LinkedList<T> {
         head = new Node<T>(data);
         size = 1;
     }
+ 
+    @Override
+    public String toString() {
+        if (head == null){
+            return "[]";
+        }
+
+        String result = "[" + head + "]";
+        
+        return result;
+    }
+
+    //get size
+    public int size(){
+        return this.size;
+    }
 
     public boolean isEmpty(){
         if (head == null){
@@ -27,31 +43,158 @@ public class LinkedList<T> {
         return false;
     }
 
-    public Node<T> getHead() {
-        return head;
-    }
-    
-    public void add(T data){
+    //insert nodes       
+    public void pushBack(T data){ //O(n)
         Node<T> newNode = new Node<T>(data);
 
         if (head == null){
             head = newNode;
+        }
+        else{
+            Node<T> currentNode = head;
+            while (currentNode.getNext() != null){
+                currentNode = currentNode.getNext();
+            }
+            currentNode.setNext(newNode);
+        }       
+        size ++;
+    }
+
+    public void pushFront(T data) { //O(n)
+        Node<T> newNode = new Node<>(data);
+        newNode.setNext(this.head);
+        this.head = newNode;
+        this.size ++;
+    }
+
+    public void insertAt(int index, T data) throws IndexOutOfBoundsException{ //O(n)
+        if (index < 0 || index > size){
+            throw new IndexOutOfBoundsException("Index out of Bounds");
+        }
+        
+        if (index == 0){
+            this.pushFront(data);
             return;
+        }
+        
+        Node<T> new_node = new Node<T>(data);
+        Node<T> prev_node = this.head;
+        Node<T> next_node = this.head.getNext();
+        for (int i = 1; i < index; i++){
+            prev_node = next_node;
+            next_node = next_node.getNext();
+        }
+        new_node.setNext(next_node);
+        prev_node.setNext(new_node);
+        size ++;
+
+
+    }
+
+    //change node value
+    public void setAt(int index, T data) throws IndexOutOfBoundsException{
+        if (index < 0 || index >= size){
+            throw new IndexOutOfBoundsException("Index out of bounds");
         }
 
         Node<T> currentNode = head;
-
-        while (currentNode.getNext() != null){
+        for(int i = 0; i < index; i++){
             currentNode = currentNode.getNext();
         }
 
-        currentNode.setNext(newNode);
+        currentNode.setData(data);
+
+    }
+    
+    //get values
+    public T getFront() {
+        return head.getData();
     }
 
-    @Override
-    public String toString() {
-        String result = "[ " + head + " ]";
-        
-        return result;
+    public boolean contains(T data){ //O(n)
+        Node<T> currentNode = head;
+        while (currentNode != null){
+            if (data.equals(currentNode.getData())){
+                return true;
+            }
+            currentNode = currentNode.getNext();
+        }
+        return false;
     }
+
+    public T getAt(int index) throws IndexOutOfBoundsException{
+        if (index < 0 || index >= size){
+            throw new IndexOutOfBoundsException("Index out of bounds");
+        }
+
+        Node<T> currentNode = head;
+        for(int i = 0; i < index; i++){
+            currentNode = currentNode.getNext();
+        }
+
+        return currentNode.getData();
+
+    }
+    
+    public int indexOf(T data){
+        Node<T> currNode = this.head;
+
+        for (int i = 0; i < size; i++){
+            if (data.equals(currNode.getData())){
+                return i;
+            }
+            currNode = currNode.getNext();
+        }
+
+        return -1;
+    }
+
+    //delete
+    public void deleteAt(int index) throws IndexOutOfBoundsException{
+        if (index < 0 || index >= size){
+            throw new IndexOutOfBoundsException("Index out of bounds");
+        }
+
+        if (index == 0){
+            head = head.getNext();
+        }
+        else{
+            Node<T> currentNode = head;
+            Node<T> prevNode = currentNode;
+
+            for (int i = 0; i < index; i++){
+                prevNode = currentNode;
+                currentNode = currentNode.getNext();
+            }
+
+            prevNode.setNext(currentNode.getNext());
+
+        }
+
+        size --;
+
+    }
+
+    public boolean delete(T data){
+        if (data.equals(head.getData())){
+            head = head.getNext();
+            size --;
+            return true;
+        }
+
+        Node<T> prev_node = this.head;
+        Node<T> curr_node = this.head.getNext();
+        while(curr_node != null){
+            if (data.equals(curr_node.getData())){
+                prev_node.setNext(curr_node.getNext());
+                size --;
+                return true;
+            }
+            prev_node = curr_node;
+            curr_node = curr_node.getNext();
+        }
+        return false;
+        
+    }
+
 }
